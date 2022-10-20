c2e260909b22fc7fe1ef49be10d2bda34912af7f
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-10-20 12:44:07 -0400

    Items on page, need functionality built in

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="container top">
+        <h1>Weather App</h1>
+        <form action="" onSubmit="return false">
+          <input
+            type="text"
+            className="search"
+            placeholder="Search by city name"
+          />
+          <button type="submit" className="btn">
+            Submit
+          </button>
+          <p className="error"></p>
+        </form>
+        <section>
+          <div className="container">
+            <div className="icon">
+              <img src="" alt="" className="weather-icon" />
+            </div>
+            <div className="location">
+              <div className="city">New York, US</div>
+              <div className="date">Thursday, Dec 26 2020</div>
+            </div>
+            <div className="current">
+              <div className="temp">
+                Temp: 25 <span>°C</span>
+              </div>
+              <div className="weather">Weather: Sunny</div>
+              <div className="temp-range">Temp Range: 10°C / 15°C</div>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
